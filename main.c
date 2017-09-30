91702c8cb00c901341c76d5d1a67e945b71dfd74
wassimoo <wassimjbaali5121997@gmail.com>
2017-09-30 19:47:31 +0100

    Initialisation

---
+++ main.c
@@ -0,0 +1,341 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <unistd.h>
+#include "main.h"
+
+static EDITOR E;
+static struct termios stored_settings;
+
+int main(int argc, char *argv[]){
+    initEditor();
+    while (1)
+    {
+        drawScreen();
+        process_key(get_key());
+    }
+    return 1;
+}
+
+void insertNewLine(){
+    E.cy++;
+    E.cx = 0;
+    E.coloff = 0;
+    E.numrows++;
+    E.row_index++;
+    E.row = realloc(E.row, E.numrows * sizeof(ROW));
+    E.row[E.row_index].chars = malloc(1);
+    E.row[E.row_index].chars[0] = '\0';
+    E.row[E.row_index].length = 1;
+    if (E.numrows > E.screen_rows)
+        E.rowoff++;
+    //TO BE CONTINUED :)
+}
+
+void write_to_file(void){
+    E.file = fopen("this.txt", "w");
+    //TODO: handle failure on opening file
+    int i;
+    for (i = 0; i < E.numrows; i++, E.row++)
+    {
+        fputs(E.row->chars, E.file);
+        fputc('\n', E.file);
+    }
+}
+
+void textAppend(struct Text *txt, char *s, int len){
+    char *new = realloc(txt->b, txt->len + len);
+
+    if (new == NULL)
+        return;
+    memcpy(new + txt->len, s, len);
+    txt->b = new;
+    txt->len += len;
+}
+
+void drawScreen(void){
+    struct Text txt = {NULL, 0};
+    textAppend(&txt, "\x1b[?25l", 6); /*Hide*/
+    textAppend(&txt, "\x1b[H", 3);    /*Go Home*/
+
+    int i;
+    int rowIndex = E.row_index;
+    for (i = 0,rowIndex=E.rowoff;i < E.screen_rows ; i++)
+    {
+        if(rowIndex < E.numrows){
+            textAppend(&txt, E.row[rowIndex].chars, E.row[rowIndex].length);
+            textAppend(&txt, "\x1b[0K\r\n", 7);
+            rowIndex++;
+        }
+        else{
+                textAppend(&txt, "~\x1b[0K\r\n", 7);
+        }
+    }
+    textAppend(&txt, "\x1b[?25h", 6); /* Show cursor. */
+    write(STDOUT_FILENO, txt.b, txt.len);
+    cursorGO(E.cy+1,E.cx+1);
+}
+
+/* This shit is fckin working */
+void move_cursor(int direction){
+    ROW *row = E.row + E.row_index;
+    switch (direction)
+    {
+    case ARROW_DOWN:
+        if (E.rowoff + E.cy < E.numrows)
+        {
+            if (E.cy == E.screen_rows - 1)
+                E.rowoff++;
+            else
+            {
+                E.cy++;
+            }
+            E.row_index++;
+        }
+        break;
+
+    case ARROW_UP:
+        if (E.cy > 0)
+        {
+            E.row_index--;
+            E.cy--;
+        }
+        else if (E.rowoff > 0)
+        {
+            E.row_index--;
+            E.rowoff--;
+        }
+
+        break;
+
+    case ARROW_RIGHT:
+
+        if (E.cx + E.coloff < row->length - 1)
+        {
+            if (E.cx == E.screen_cols - 1)
+                E.coloff++;
+            else
+                E.cx++;
+        }
+        else if (E.cx + E.coloff == row->length - 1) /*Let's try to jump to next line */
+        {
+            if (E.row_index < E.numrows - 1)
+            {
+                E.cx = 0;
+                E.coloff = 0;
+                if (E.cy + E.rowoff == E.screen_rows - 1)
+                    E.rowoff++;
+                else
+                {
+                    E.cy++;
+                }
+                E.row_index++;
+            }
+        }
+        break;
+
+    case ARROW_LEFT:
+        if (E.cx > 0)
+            E.cx--;
+        else
+        { /*On the left Edge */
+            if (E.coloff > 0)
+                E.coloff--;
+            else
+            { /*Go up */
+                if (E.row_index > 0)
+                {
+                    E.cx = E.row[E.row_index - 1].length - 1;
+                    if (E.cy == 0)
+                    {
+                        if (E.rowoff > 0)
+                            E.rowoff--;
+                    }
+                    else
+                        E.cy--;
+                    E.row_index--;
+                    if (E.cx > E.screen_cols - 1)
+                    {
+                        E.coloff = E.cx - E.screen_cols + 1;
+                        E.cx = E.screen_cols - 1;
+                    }
+                }
+            }
+        }
+        break;
+    }
+    /*Move cursor to last char if it's out of string due to Up and Down Arrows*/
+    row = E.row + E.row_index;
+    if (E.cx > row->length - 1)
+    {
+        E.cx = row->length - 1; //TODO : Handle offset screen cols
+    }
+    cursorGO(E.cy + 1, E.cx + 1);
+}
+
+/*Delete one single char at a specific position */
+void deletechar(int pos, ROW *row){
+    memmove(row->chars + pos, row->chars + pos + 1, row->length - pos);
+    row->length--;
+    E.cx--;
+}
+
+void deleteProcess(ROW *row, ROW *prevRow) /*incomplet (a little bit complicated)*/{
+    if (E.cx > 0)
+    { /*we have somthing to delete*/
+        deletechar(E.cx - 1, row);
+    }
+    else if (E.cy > 0)
+    { /*handle the case where there's no line suppression*/
+        resizeString(prevRow->chars,
+                     prevRow->length + row->length - 1);                        /*-1 for one single null term*/
+        memmove(prevRow->chars + prevRow->length - 2, row->chars, row->length); /*-2 to move null term + newline term*/
+        prevRow->length = prevRow->length + row->length - 1;
+
+        /*move rows by 1 position to the left and free() last row */
+        if (E.row_index > E.numrows - 1)
+            row = memmove(row, E.row + E.row_index + 1, E.numrows - E.row_index - 1);
+
+        cursorup(1);
+        cursorforward(prevRow->length);
+        E.row_index--;
+        E.numrows--;
+    }
+}
+
+char *resizeString(char *chars, int newSize){
+    char *tmp;
+    if (!(tmp = realloc(chars, newSize)))
+        exit(-2); /*Could not alloc enough space for row*/
+    return tmp;
+}
+
+void insertChar(char c){ 
+    /*-exec p 'main.c'::E.row+1*/
+    /*ROW *debug_r = E.row;*/
+
+    E.row[E.row_index].chars = resizeString(E.row[E.row_index].chars, E.row[E.row_index].length + 1);
+    E.row[E.row_index].chars[E.row[E.row_index].length - 1] = c;
+    E.row[E.row_index].chars[E.row[E.row_index].length] = '\0';
+    // printf("%c", E.row[E.row_index].chars[E.row[E.row_index].length - 1]); //(printf("%c",c))
+    E.row[E.row_index].length++;
+    /* if (E.cx == E.screen_cols)
+        {
+            cursordown(1);
+            cursorbackward(E.row[E.row_index].length);
+            E.cx = 0;
+            E.cy++;
+        }
+        else*/
+    E.cx++;
+}
+
+int get_key(void){
+    char c;
+    c = fgetc(stdin);
+    if (c == 'z')
+    {
+        write_to_file();
+        echo_on();
+        exit(0);
+    }
+
+    else if (c == ESC)
+    {
+        getchar();
+        c = getchar();
+        /*escape sequence */
+        switch (c)
+        {
+        case 'A':
+            return ARROW_UP;
+        case 'B':
+            return ARROW_DOWN;
+        case 'C':
+            return ARROW_RIGHT;
+        case 'D':
+            return ARROW_LEFT;
+        case 'H':
+            return HOME_KEY;
+        case 'F':
+            return END_KEY;
+        }
+    }
+    else
+    {
+        return c;
+    }
+}
+
+void process_key(int key){
+    switch (key)
+    {
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+    case ARROW_UP:
+    case ARROW_DOWN:
+        move_cursor(key);
+        break;
+    case BACKSPACE:
+        printf("\b");
+        printf(" ");
+        printf("\b");
+        if (E.cx > 0)
+            deleteProcess(E.row + E.row_index, NULL);
+        else
+            deleteProcess(E.row + E.row_index, E.row + E.row_index - 1);
+        break;
+    case ENTER:
+        insertNewLine();
+        cursorGO(E.cy + 1, E.cx + 1);
+        break;
+    default:
+        insertChar(key);
+        break;
+    }
+}
+
+void initEditor(){
+    echo_off();
+    E.row = calloc(1, sizeof(E));
+    E.row->length = 1;
+    E.row_index = 0;
+    E.row->chars = malloc(sizeof(char));
+    E.row->chars[0] = '\0';
+    E.row_index = 0;
+    E.cx = 0;
+    E.cy = 0;
+    E.rowoff = 0;
+    E.coloff = 0;
+    E.numrows = 1;
+    struct winsize w;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &w);
+    E.screen_rows = w.ws_row;
+    E.screen_cols = w.ws_col;
+}
+
+void echo_off(void){
+    struct termios new_settings;
+    tcgetattr(0, &stored_settings);
+    new_settings = stored_settings;
+    new_settings.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    new_settings.c_cc[VMIN] = 1;
+    new_settings.c_cc[VTIME] = 1;
+    tcsetattr(0, TCSANOW, &new_settings);
+    return;
+}
+
+void echo_on(void){
+    tcsetattr(0, TCSANOW, &stored_settings);
+    return;
+}
+
+/*================================== Known Bugs List ================================== 
+    * if deleting starts from the above row it crushs on the upper row;;;;;;;
+    * Handle the TAB Key:
+        * diffrence between cursor and E.cx (think of updating E.cx to current cursor position on TAB click)
+        * Update spacing between chars if tab is insertd in the middle of Line ;
+    * Handle on screen size change (Update E.screen_rows and E.screen_cols and redraw screen);;;;;
+ ================================== Known Bugs List ==================================*/
---
+++ main.h
@@ -0,0 +1,71 @@
+#define cursorup(x) printf("\033[%dA", (x))
+#define cursordown(x) printf("\033[%dB", (x))
+#define cursorforward(x) printf("\033[%dC", (x))
+#define cursorbackward(x) printf("\033[%dD", (x))
+#define cursorGO(y,x) printf("\033[%d;%dH", y , x)
+enum KEY_ACTION
+{
+    KEY_NULL = 0,    /* NULL */
+    CTRL_C = 3,      /* Ctrl-c */
+    CTRL_D = 4,      /* Ctrl-d */
+    CTRL_F = 6,      /* Ctrl-f */
+    CTRL_H = 8,      /* Ctrl-h */
+    TAB = 9,         /* Tab */
+    CTRL_L = 12,     /* Ctrl+l */
+    ENTER = 10,      /* Enter */
+    CTRL_Q = 17,     /* Ctrl-q */
+    CTRL_S = 19,     /* Ctrl-s */
+    CTRL_U = 21,     /* Ctrl-u */
+    ESC = 27,        /* Escape */
+    BACKSPACE = 127, /* Backspace */
+    /* The following are just soft codes, not really reported by the
+         * terminal directly. */
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
+typedef struct
+{
+    u_int length;
+    char *chars;
+} ROW;
+
+typedef struct
+{
+    u_int cx,
+        cy, /*Cursor position */
+        screen_rows,
+        screen_cols,
+        rowoff,  //To the up
+        coloff, //To the left
+        numrows;
+    ROW *row;
+    u_int row_index;
+    FILE *file;
+} EDITOR;
+
+struct Text {
+    char *b;
+    int len;
+};
+
+void textAppend(struct Text *txt, char *s, int len);
+void drawScreen(void);
+char *resizeString(char *chars ,int newSize);
+void process_key(int key);
+void insertNewLine();
+void insertChar(char c);
+void initEditor();
+void deleteProcess(ROW* row,ROW* prevRow);
+int get_key(void);
+void write_to_file(void);
+void move_cursor(int direction);
+void echo_on(void);
+void echo_off(void);
